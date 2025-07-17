8aa6bc26523418c1493924dfb0f3ef526de2bb24
Raks <rakioths@gmail.com>
2025-07-17 14:21:53 +0200

    ⬆️ Bump SDK

--- buildSrc/src/main/kotlin/Apps.kt
+++ buildSrc/src/main/kotlin/Apps.kt
@@ -2,11 +2,11 @@ import org.gradle.api.JavaVersion
 
 object Apps {
     const val APPLICATION_ID               = "com.raks.pvcreator"
-    const val COMPILE_SDK                  = 34
+    const val COMPILE_SDK                  = 35
     const val MIN_SDK                      = 26
-    const val TARGET_SDK                   = 34
-    const val VERSION_CODE                 = 3
-    const val VERSION_NAME                 = "1.0.2"
+    const val TARGET_SDK                   = 35
+    const val VERSION_CODE                 = 4
+    const val VERSION_NAME                 = "1.0.3"
     const val JUNIT_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
     const val HILT_INSTRUMENTATION_RUNNER  = "${APPLICATION_ID}.HiltTestRunner"
     const val IS_MINIFY_ENABLED            = true
